8d70ef2660f4703069acb7417a7fb36526f0deb1
Hugo Bullows Weeks <hugobullowsweeks1@gmail.com>
2025-07-07 16:57:26 +0100

    rename package directory

---
+++ fusion_cashflow_app/__init__.py
@@ -0,0 +1,9 @@
+"""Fusion cashflow analysis package."""
+
+__all__ = [
+    "cashflow_engine",
+    "visuals",
+    "report",
+    "dashboard",
+    "app",
+]
---
+++ fusion_cashflow_app/visuals/__init__.py
@@ -0,0 +1,4 @@
+"""Visualization utilities."""
+from . import bokeh_plots
+
+__all__ = ["bokeh_plots"]
